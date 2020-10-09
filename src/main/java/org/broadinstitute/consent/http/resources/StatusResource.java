2955121ff917679ccac3d525e3b582f165d67da5
Greg Rushton <grushton@broadinstitute.org>
2020-10-09 12:46:07 -0400

    add a degraded status to the status check

--- src/main/java/org/broadinstitute/consent/http/resources/StatusResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/StatusResource.java
@@ -1,54 +1,126 @@
 package org.broadinstitute.consent.http.resources;
 
-import com.codahale.metrics.health.HealthCheck;
-import com.codahale.metrics.health.HealthCheckRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.broadinstitute.consent.http.ConsentModule.DB_ENV;
 
+import com.codahale.metrics.health.HealthCheck;
+import com.codahale.metrics.health.HealthCheck.Result;
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.Map;
-
-import static org.broadinstitute.consent.http.ConsentModule.DB_ENV;
 
 @Path("status")
 public class StatusResource {
 
-    private Logger logger() {
-        return LoggerFactory.getLogger(this.getClass());
+  private final HealthCheckRegistry healthChecks;
+
+  public StatusResource(HealthCheckRegistry healthChecks) {
+    this.healthChecks = healthChecks;
+  }
+
+  @GET
+  @Produces("application/json")
+  public Response getStatus() {
+    Map<String, HealthCheck.Result> results = healthChecks.runHealthChecks();
+    StatusCheckResponse response = formatResponse(results);
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    return Response.ok(gson.toJson(response)).build();
+  }
+
+  private StatusCheckResponse formatResponse(Map<String, HealthCheck.Result> results) {
+    List<SubsystemStatus> subsystemStatuses =
+        results.entrySet().stream()
+            .map(
+                entry ->
+                    new SubsystemStatus(
+                        entry.getKey(), entry.getValue().isHealthy(), entry.getValue()))
+            .collect(Collectors.toList());
+
+    // OK status is valid as long as the database is up.
+    boolean ok = subsystemStatuses.stream().anyMatch(s -> s.ok && s.name.equals(DB_ENV));
+    // If any subsystem is not OK, we consider the system degraded
+    boolean degraded = subsystemStatuses.stream().anyMatch(s -> !s.ok);
+    Map<String, Result> systems =
+        subsystemStatuses.stream()
+            .collect(Collectors.toMap(SubsystemStatus::getName, SubsystemStatus::getResult));
+    return new StatusCheckResponse(ok, degraded, systems);
+  }
+
+  private static class StatusCheckResponse {
+    private boolean ok;
+    private boolean degraded;
+    private Map<String, Result> systems;
+
+    public StatusCheckResponse(boolean ok, boolean degraded, Map<String, Result> systems) {
+      this.ok = ok;
+      this.degraded = degraded;
+      this.systems = systems;
     }
 
-    private final HealthCheckRegistry healthChecks;
-
-    public StatusResource(HealthCheckRegistry healthChecks) {
-        this.healthChecks = healthChecks;
+    public boolean isOk() {
+      return ok;
     }
 
-    @GET
-    @Produces("application/json")
-    public Response getStatus() {
-        Map<String, HealthCheck.Result> results = healthChecks.runHealthChecks();
-        HealthCheck.Result postgresql = results.getOrDefault(DB_ENV, HealthCheck.Result.unhealthy("Unable to access postgresql database"));
-        if (postgresql.isHealthy()) {
-            return Response.ok(results).build();
-        } else {
-            results.entrySet().
-                    stream().
-                    filter(e -> !e.getValue().isHealthy()).
-                    forEach(e -> logger().error("Error in service " + e.getKey() + ": " + formatResultError(e.getValue())));
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(results).build();
-        }
+    public void setOk(boolean ok) {
+      this.ok = ok;
     }
 
-    private String formatResultError(HealthCheck.Result result) {
-        if (result.getMessage() != null) {
-            return result.getMessage();
-        } else if (result.getError() != null) {
-            return result.getError().toString();
-        }
-        return "Healthcheck Result Error";
+    public boolean isDegraded() {
+      return degraded;
     }
 
+    public void setDegraded(boolean degraded) {
+      this.degraded = degraded;
+    }
+
+    public Map<String, Result> getSystems() {
+      return systems;
+    }
+
+    public void setSystems(Map<String, Result> systems) {
+      this.systems = systems;
+    }
+  }
+
+  private static class SubsystemStatus {
+    String name;
+    boolean ok;
+    Result result;
+
+    public SubsystemStatus(String name, boolean ok, Result result) {
+      this.name = name;
+      this.ok = ok;
+      this.result = result;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public boolean isOk() {
+      return ok;
+    }
+
+    public void setOk(boolean ok) {
+      this.ok = ok;
+    }
+
+    public Result getResult() {
+      return result;
+    }
+
+    public void setResult(Result result) {
+      this.result = result;
+    }
+  }
 }
